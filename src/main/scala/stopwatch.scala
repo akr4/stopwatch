558f042aa9262405e434c626749d1b6a8662fdd4
Akira Ueda <ue@gol.com>
2012-01-25 19:19:37 +0900

    README, license descriptions

--- src/main/scala/stopwatch.scala
+++ src/main/scala/stopwatch.scala
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 Akira Ueda
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.physalis.stopwatch
 
 case class Section(taskName: String, startTime: Long)
@@ -27,7 +42,7 @@ class EventLogger extends StopWatchEventListener {
  *     }
  *
  * In thie case, EventLogger is used as the default StopWatchEventListener,
- * and it put log entries through SLF4J "stopwatch" category.
+ * and it put a log entry through SLF4J "stopwatch" category.
  *
  * You can start and stop explicitly::
  *
